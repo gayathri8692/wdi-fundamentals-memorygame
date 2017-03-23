19a2ad6d8b317d4e6a6c7f6a629191b31d65d5c8
Gayathri <gayathri@Srirams-MBP-2.fios-router.home>
2017-03-22 20:24:55 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,21 +2,26 @@ console.log("Up and running!!");
 
 var cards = ["queen" , "queen" , "king" , "king"]
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]){
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!!");
 	} else {
 	alert("Sorry, try again.");
 }
+
+
 }
 
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 
+if (cardsInPlay.length === 2) {
+	checkForMatch();
 
-console.log("User Flipped " + cardTwo);
+}
+}
+
+flipCard(0);
+flipCard(2);
+
